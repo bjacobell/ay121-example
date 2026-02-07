f70a88ac4dcc02d0c83c6c79a02c0e540a01971d
bjacobell <97710840+bjacobell@users.noreply.github.com>
2026-02-06 16:09:49 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='py121example',
     version="0.1.0",
-    packages=['py121example'],
+    packages=find_packages(),
     install_requires=[
         'numpy'
     ],
